2a267233ac802621b96b9f88817ca033dc5abdc3
rzfhlv <rzfhlv.me@gmail.com>
2023-05-13 22:49:42 +0700

    dockerize

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Pegasus</title>
+
+        <!-- Favicon -->
+        <link rel="icon" href="{{ url('laravel-icon.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
